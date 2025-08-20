5c2bb5fb924ea201ef4da3e0b43c0c9d12432580
Gulya Aliniyozova <gulyaaliniyozova@gmail.com>
2025-08-20 16:21:55 +0500

    My homework

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 
+// const 
+const bookTitle ="Мастер и Маргарита";
+const author ="Михаил Булгаков";
+const year =1967;
+
+// let
+let isRead=true;
+let comment ="Одна из самых лучшых книга в 20 века"
+
+console.log("название:", bookTitle);
+console.log("Автор:", author);
+console.log("Год издания:", year);
+console.log("Статус:", isRead);
+console.log("Коментарии", comment);
+
+console.log(" Tip bookTitle:", typeof bookTitle);
+console.log("Tip author:" , typeof author);
+console.log("Tip year:", typeof year );
+console.log("Tip isRead: ", typeof isRead);
+console.log("Tip comment:" , typeof comment);
